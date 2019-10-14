55ae5a44e81652294df30f3260ce2b254c379def
Thomas Zobrosky <thomaszobrosky@gmail.com>
2019-10-14 01:39:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,13 @@ return nil
 end
 
 def find_max_value(array)
-  # Add your solution here
+maxNum = 0 
+  array.each {|items|
+  if items > maxNum
+    maxNum = items
+  end
+return items
+}
 end
 
 def find_min_value(array)
